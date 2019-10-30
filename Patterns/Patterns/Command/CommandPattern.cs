632caa9bf7de56cfe2d2bcc84a0b1b8489029b44
Bob Flynn <BFlynn@uwcu.org>
2019-10-30 16:28:38 -0500

    add init command pattern

---
+++ Patterns/Patterns/Command/CommandPattern.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Patterns
+{
+    //Command Pattern:
+    //Encapsulates requests 
+    //Inject that command object into an invoker
+    //The invoker class can be as fancy or as simple as needed (undo, repeat, batch commands etc...)
+    //The concrete commands act on a receiver
+    //command classes are usually limited to specific actions some support undo functionality
+
+    //Command contract
+    public interface ICommand
+    {
+        void Execute();
+        //often an undo command is added in
+        void Undo();
+    }
+
+    //Concrete command
+    public class TurnOnLightCommand : ICommand
+    {
+        private Lightbulb lb;
+
+        public TurnOnLightCommand(Lightbulb lb)
+        {
+            this.lb = lb;
+        }
+
+        public void Execute()
+        {
+            lb.IsOn = true;
+        }
+
+        public void Undo()
+        {
+            lb.IsOn = false;
+        }
+    }
+
+    #region Receiver
+    //Receiver -- Something commands can act on
+    public class Lightbulb : ILight
+    {
+        public Lightbulb()
+        {
+            IsOn = false;
+        }
+
+        public bool IsOn { get; set; }
+    }
+
+    //Just an interface for Lightbulbs, not part of pattern
+    public interface ILight
+    {
+        bool IsOn { get; set; }
+    }
+    #endregion
+
+    //Invoker -- Coordinates commands
+    public class CommandInvoker
+    {
+        public static Queue<ICommand> commandStack = new Queue<ICommand>();
+        public static Stack<ICommand> commandHistory = new Stack<ICommand>();
+
+        public void AddCommand(ICommand command)
+        {
+            commandStack.Enqueue(command);
+        }
+
+        public void ExecuteNextCommand()
+        {
+            var currentCommand = commandStack.Peek();
+            currentCommand.Execute();
+            commandStack.Dequeue();
+            commandHistory.Push(currentCommand);
+        }
+
+        public void UndoLastCommand()
+        {
+            var command = commandHistory.Pop();
+            command.Undo();
+        }
+    }
+}
---
+++ Patterns/PatternsTests/CommandTests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Patterns;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PatternsTests
+{
+    [TestClass]
+    public class CommandTests
+    {
+        [TestMethod]
+        public void Command_LightBulb_ExecuteNextCommand()
+        {
+            //Arrange
+            var commandInvoker = new CommandInvoker();
+            var receiver = new Lightbulb();
+            Assert.IsFalse(receiver.IsOn);
+
+            //Act
+            commandInvoker.AddCommand(new TurnOnLightCommand(receiver));
+            commandInvoker.ExecuteNextCommand();
+
+            //Assert
+            Assert.IsTrue(receiver.IsOn);
+        }
+
+        [TestMethod]
+        public void Command_LightBulb_UndoLastCommand()
+        {
+            //Arrange
+            var commandInvoker = new CommandInvoker();
+            var receiver = new Lightbulb();
+            Assert.IsFalse(receiver.IsOn);
+
+            //Act
+            commandInvoker.AddCommand(new TurnOnLightCommand(receiver));
+            commandInvoker.ExecuteNextCommand();
+            Assert.IsTrue(receiver.IsOn);
+
+            commandInvoker.UndoLastCommand();
+            Assert.IsFalse(receiver.IsOn);
+        }
+    }
+}
